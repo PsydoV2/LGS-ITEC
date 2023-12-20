0a5046f38a27eba503bc508f9caa5832912ea2e8
PsydoV2 <seb.falter@gmail.com>
2023-12-20 20:21:57 +0100

    fix

--- uninstall.sh
+++ uninstall.sh
@@ -3,5 +3,5 @@ rm /usr/local/bin/testping
 if [ ! -f "/usr/local/bin/testping" ]; then
     echo "Erfolgreich entfernt!"
 else
-    echo "Konnte nicht entfernt werden!"
+    echo "Fehler beim Entfernen!"
 fi 
