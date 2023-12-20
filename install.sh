2b9ae58bec33bf2ae2bc17b565b6be65af51944d
PsydoV2 <seb.falter@gmail.com>
2023-12-20 20:25:53 +0100

    adding manpage

--- install.sh
+++ install.sh
@@ -2,7 +2,9 @@
 
 if [ -f "src/main.sh" ]; then
     cp src/main.sh /usr/local/bin/testping
-    rm src
+    cp src/testping.1 /usr/share/man/man1/
+    mandb
+    # rm src
 
     if [ -f "/usr/local/bin/testping" ]; then
         chmod +x /usr/local/bin/testping 
--- uninstall.sh
+++ uninstall.sh
@@ -1,4 +1,5 @@
 rm /usr/local/bin/testping
+rm /usr/share/man/man1/testping.1
 
 if [ ! -f "/usr/local/bin/testping" ]; then
     echo "Erfolgreich entfernt!"
