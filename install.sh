cd6ae26b76183776ea7401a330237eae632e90cc
PsydoV2 <seb.falter@gmail.com>
2023-12-20 14:18:36 +0100

    creating installation

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Verschiebe die Datei main.sh nach /bin/usr/command/ (falls sie dort noch nicht vorhanden ist)
+mv /command/main.sh /bin/usr/command/
+
+# Wechsel in das Verzeichnis /bin/usr/command/
+cd /bin/usr/command/
+
+# Überprüfe, ob main.sh im Ordner existiert
+if [ -f "main.sh" ]; then
+    # Ändere die Berechtigungen von main.sh, um sie ausführbar zu machen
+    chmod +x main.sh
+    echo "Installiert"
+else
+    echo "main.sh existiert nicht im Ordner /bin/usr/command/"
+fi
