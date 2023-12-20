73d8ca985e13257fcaa7e2a1cd5995a75d471d09
PsydoV2 <seb.falter@gmail.com>
2023-12-20 20:12:16 +0100

    fixing

--- install.sh
+++ install.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
 
-# Überprüfen, ob der Ordner bereits existiert, wenn nicht, erstellen
-if [ ! -d "usr/bin/testping" ]; then
-    mkdir -p usr/bin/testping
-fi
-
-# Datei wird verschoben
 if [ -f "src/main.sh" ]; then
-    mv src/main.sh usr/bin/testping/
+    # Verschieben von main.sh nach /usr/local/bin/testping
+    mv src/main.sh /usr/local/bin/testping
 
-    if [ -f "usr/bin/testping/main.sh" ]; then
-        chmod +x usr/bin/testping/main.sh
-        echo "Skript erfolgreich installiert."
+    # Überprüfen, ob die Datei erfolgreich verschoben wurde
+    if [ -f "/usr/local/bin/testping" ]; then
+        chmod +x /usr/local/bin/testping  # Setzen der Ausführungsrechte
+        echo "Das Skript 'main.sh' wurde erfolgreich als 'testping' konfiguriert."
     else
-        echo "Skript konnte nicht installiert werden."
+        echo "Fehler beim Verschieben des Skripts nach '/usr/local/bin/testping'."
     fi
 else
     echo "Die Datei main.sh wurde im Verzeichnis src nicht gefunden."
